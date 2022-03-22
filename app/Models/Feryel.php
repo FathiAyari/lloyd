78eb2a8af0f73e210c567eae88374d4850dd83d3
FathiAyari <fathi3ayari333@gmail.com>
2022-03-22 11:57:34 +0100

    1er commit

---
+++ app/Models/Feryel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Feryel extends Model
+{
+    use HasFactory;
+}
